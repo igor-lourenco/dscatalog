37a115d9ad92f6a18782217c1e83fa6cdc73c5d0
igor-lourenco <igor.lourencosantos@gmail.com>
2021-07-10 13:04:22 -0300

    Format price

--- frontweb/src/components/ProductPrice/index.tsx
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import {formatPrice} from 'util/formatters';
  type Props = {
      price:number;
  }
@@ -7,7 +8,7 @@ const ProductPrice = ({price} : Props) => {
     return (
         <div className="product-price-container">
             <span>R$</span>
-            <h3>{price}</h3>
+            <h3>{formatPrice(price)}</h3>
         </div>
     );
 }
---
+++ frontweb/src/util/formatters.ts
@@ -0,0 +1,6 @@
+export const formatPrice = (price : number) => {
+
+    const params = {minimumFractionDigits: 2, maximumFractionDigits: 2};
+    return new Intl.NumberFormat('pt-BR', params).format(price);
+    
+}
