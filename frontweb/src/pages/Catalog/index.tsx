b85a83892bc1f61e13de6b504e56682ce7abc0d8
igor-lourenco <igor.lourencosantos@gmail.com>
2021-07-12 18:23:36 -0300

    catalog title

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,6 +1,7 @@
 import ProductCard from 'components/ProductCard';
 import { Product } from 'types/product';
 import { Link } from 'react-router-dom';
+import './styles.css'
 
 const Catalog = () => {
   const product: Product = {
@@ -21,34 +22,37 @@ const Catalog = () => {
   };
 
   return (
-    <div className="container my-4">
+    <div className="container my-4 catalog-container">
+      <div className="row catalog-title-container">
+        <h1>Catálogo de produtos</h1>
+      </div>
+
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
-          <ProductCard product={product} />
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
-          <ProductCard product={product} />
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
-          <ProductCard product={product} />
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
-          <ProductCard product={product} />
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
-          <ProductCard product={product} />
+            <ProductCard product={product} />
           </Link>
         </div>
-        
       </div>
     </div>
   );
