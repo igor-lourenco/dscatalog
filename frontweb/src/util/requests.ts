4e358fc33fa035c21cf48f0ece44a111dd91fc23
igor-lourenco <igor.lourencosantos@gmail.com>
2021-07-21 13:23:59 -0300

    Has any roles

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -4,99 +4,139 @@ import history from './history';
 import jwtDecode from 'jwt-decode';
 
 type LoginResponse = {
-    access_token: string;
-    token_type:string;
-    expires_in: number;
-    scope: string;
-    userFirstName: string;
-    userId: number;
-}
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
 export type TokenData = {
-    exp : number;
-    user_name : string;
-    authorities: Role[];
-}
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+};
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
-const basicHeader = () => 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET);
+const basicHeader = () =>
+  'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET);
 const tokenKey = 'authData';
 
 type LoginData = {
-    username : string;
-    password: string;
-}
+  username: string;
+  password: string;
+};
 
 export const requestBackendLogin = (loginData: LoginData) => {
-    const headers = {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Authorization' : basicHeader()
-    }
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: basicHeader(),
+  };
 
-    const data = qs.stringify( {
-        ...loginData,
-        grant_type : 'password'
-    })
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password',
+  });
 
-    return axios({method: 'POST', baseURL: BASE_URL, url : '/oauth/token', data, headers});
-}
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
+};
 
 export const requestBackend = (config: AxiosRequestConfig) => {
-    const headers = config.withCredentials ? {
+  const headers = config.withCredentials
+    ? {
         ...config.headers,
-        Authorization: "Bearer " + getAuthData().access_token //acrescenta o token no corpo
-    } : config.headers; //se der erro
+        Authorization: 'Bearer ' + getAuthData().access_token, //acrescenta o token no corpo
+      }
+    : config.headers; //se der erro
 
-    return axios({...config, baseURL: BASE_URL, headers });
-}
+  return axios({ ...config, baseURL: BASE_URL, headers });
+};
 
-export const saveAuthData = (obj: LoginResponse) => { //salva os dados da autorização
-    return localStorage.setItem(tokenKey, JSON.stringify(obj)); //pega o objeto e transforma em string
-}
+export const saveAuthData = (obj: LoginResponse) => {
+  //salva os dados da autorização
+  return localStorage.setItem(tokenKey, JSON.stringify(obj)); //pega o objeto e transforma em string
+};
 
-export const getAuthData = () => { //retorna o dado do tipo LoginResponse
-    const str = localStorage.getItem(tokenKey) ?? "{}";
-    return JSON.parse(str) as LoginResponse;
-}
-
+export const getAuthData = () => {
+  //retorna o dado do tipo LoginResponse
+  const str = localStorage.getItem(tokenKey) ?? '{}';
+  return JSON.parse(str) as LoginResponse;
+};
 
 export const removeAuthData = () => {
-    localStorage.removeItem(tokenKey) //remove o token do localStorage
-}
+  localStorage.removeItem(tokenKey); //remove o token do localStorage
+};
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(
+  function (config) {
     return config;
-  }, function (error) {
+  },
+  function (error) {
     return Promise.reject(error);
-  });
+  }
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(
+  function (response) {
     return response;
-  }, function (error) {
-    if(error.response.status === 401 || error.response.status === 403 ){
-    history.push('/admin/auth');
-}
-    return Promise.reject(error);
-  });
-
-export const getTokenData = () : TokenData | undefined => {
-    const LoginResponse = getAuthData(); //pega o token salvo e guarda na variavel
-    try{
-    return jwtDecode(LoginResponse.access_token) as TokenData;
-    }catch(error) {
-        return undefined;
+  },
+  function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      history.push('/admin/auth');
     }
-}
+    return Promise.reject(error);
+  }
+);
 
-export const isAuthenticated = () : boolean => {
-    const tokenData = getTokenData(); //pega os dados do token 
-    return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;   
-}
+export const getTokenData = (): TokenData | undefined => {
+  const LoginResponse = getAuthData(); //pega o token salvo e guarda na variavel
+  try {
+    return jwtDecode(LoginResponse.access_token) as TokenData;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+export const isAuthenticated = (): boolean => {
+  const tokenData = getTokenData(); //pega os dados do token
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
+
+export const hasAnyRoles = (roles: Role[]): boolean => {
+  if (roles.length === 0) {
+    //testa se o usuario possui algum role
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  if (tokenData !== undefined) {
+    //testa se o token não for indefinido
+    for (var i = 0; i < roles.length; i++) {
+      //percorre a lista de roles
+      if (tokenData.authorities.includes(roles[i])) {
+        //testa se tem algum role na posição i
+        return true;
+      }
+    }
+    //return roles.some(role => tokenData.authorities.includes(role));
+  }
+  return false;
+
+};
