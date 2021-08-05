849a0585325d5663329856310cff30231971dd00
igor-lourenco <igor.lourencosantos@gmail.com>
2021-08-05 14:25:59 -0300

    Pagination layout

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,17 +1,25 @@
 import './styles.css';
 import { ReactComponent as Arrowicon } from 'assets/images/Arrow.svg';
+import ReactPaginate from 'react-paginate';
 
 const Pagination = () => {
   return (
-    <div className="pagination-container">
-      <Arrowicon  className="arrow-previous arrow-inative"/>
-      <div className="pagination-item active">1</div>
-      <div className="pagination-item">2</div>
-      <div className="pagination-item">3</div>
-      <div className="pagination-item">...</div>
-      <div className="pagination-item">10</div>
-      <Arrowicon className="arrow-next arrow-active"/>
-    </div>
+    <>
+    <ReactPaginate
+      pageCount={10} // Quantidade Total de páginas 
+      pageRangeDisplayed={3} // Quantas bolinhas da paginação vai aparecer
+      marginPagesDisplayed={1} // Quantas bolinas de paginação vao aparecer depois dos 3 pontos 
+      containerClassName="pagination-container" //classe do container que vai ter os elementos
+      pageLinkClassName="pagination-item" //estilo de cada um dos links de paginação
+      breakClassName="pagination-item" //estilo dos 3 pontos da paginação
+      previousClassName="arrow-previous" // estilo da seta
+      nextClassName="arrow-next" //estilo da seta
+      activeLinkClassName="pagination-link-active" //estilo de quanto estiver na página
+      previousLabel={<Arrowicon/>} //seta antes da paginação
+      nextLabel={<Arrowicon/>} //seta depois da paginação
+      disabledClassName="arrow-inative"
+    />
+    </>
   );
 };
 export default Pagination;
