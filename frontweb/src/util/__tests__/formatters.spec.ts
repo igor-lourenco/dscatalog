1809407ea143e782d91b9c566883c1b355702be1
Igor Lourenco <igor.lourencosantos@gmail.com>
2021-08-30 16:00:03 -0300

    First test

---
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -0,0 +1,12 @@
+import { formatPrice } from '../formatters';
+
+test('formatPrice should format number pt-BR when given 10.1', () => {
+  //ARRANGE
+  const value = 10.1;
+
+  //ACT
+  const result = formatPrice(value);
+
+  //ASSERT
+  expect(result).toEqual('10,10');
+});
