f31a1f3a641e0c6d1c435cebc17898c2023ddf9b
Igor Lourenco <igor.lourencosantos@gmail.com>
2021-08-31 16:10:12 -0300

    data-testid demonstration

--- frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -6,6 +6,8 @@ test('ButtonIcon should render button with given test', () => {
 
     render(<ButonIcon text={text} />);
 
+
     //screen.debug();
     expect(screen.getByText(text)).toBeInTheDocument();
+    expect(screen.getByTestId("arrow")).toBeInTheDocument();
 })
