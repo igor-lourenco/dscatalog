e62360b64b8be6b23538511c1edf72109bf8a1fc
Igor Lourenco <igor.lourencosantos@gmail.com>
2021-08-31 18:58:54 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -1,7 +1,12 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import Catalog from '..';
 import { Router } from 'react-router-dom';
 import history from '../../../util/history';
-import Catalog from '..';
+import {server} from './fixtures';
+
+beforeAll(() => server.listen()); //antes de executar os testes
+afterEach(() => server.resetHandlers());//depois de cada teste
+afterAll(() => server.close()); //depois que terminar os testes do arquivo
 
 describe('Catalog tests',() => {
   test('should render Catalog with products', async () => {
@@ -14,7 +19,7 @@ describe('Catalog tests',() => {
     expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
 
     await waitFor(() => {
-      expect(screen.getByText('Smart TV')).toBeInTheDocument();
+      expect(screen.getByText('PC Gamer Foo')).toBeInTheDocument();
     });
   });
 });
