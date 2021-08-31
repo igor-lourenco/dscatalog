f3efdd5063ec3ac572a0f244a9058aca82770cdf
Igor Lourenco <igor.lourencosantos@gmail.com>
2021-08-31 13:03:05 -0300

    Mocking function with spyOn

---
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,21 @@
+import { hasAnyRoles} from '../auth';
+import * as TokenModule from '../token';
+
+describe('hasAnyRole tests', () => {
+    test('should return true when empty list', () =>{
+        const result =hasAnyRoles([]);
+        expect(result).toEqual(true);
+    });
+
+    test('should return true when user has given role', () =>{
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({ //faz uma simulação do retorno da função do  getTokenData, dados mockado
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR']
+        })
+
+        const result =hasAnyRoles(['ROLE_ADMIN']);
+        expect(result).toEqual(true);
+    });
+});
