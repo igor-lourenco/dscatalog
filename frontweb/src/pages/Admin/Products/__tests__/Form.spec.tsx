47b5e8e1c56afbf706db17490f6659d89d433af4
Igor Lourenco <igor.lourencosantos@gmail.com>
2021-09-03 20:35:05 -0300

    should clear validation messages when filling out the correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -59,6 +59,7 @@ describe('Product from create tests', () => {
 
     expect(history.location.pathname).toEqual('/admin/products'); //testa o caminho redirecionado quando cadastra com sucesso
   });
+
   test('should show 5 validation message when just clicking submit', async () => {
     render(
       <Router history={history}>
@@ -74,8 +75,49 @@ describe('Product from create tests', () => {
     userEvent.click(submitButton);
 
     await waitFor(() => {
-      const message = screen.getAllByAltText('Campo obrigatório');
-      expect(message).toHaveLength(5);
+      const message = screen.getAllByText('Campo obrigatório'); // testa se tem algum lugar com esse texto
+      expect(message).toHaveLength(5); //testa se tem 5 campos
     });
+  });
+
+  test('should clear validation messages when filling out the correctly', async () => {
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    );
+
+    //screen.debug();
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i }); //pega o botao com o nome salvar
+
+    userEvent.click(submitButton); //clica no botão 
+
+    await waitFor(() => {
+      const message = screen.getAllByText('Campo obrigatório'); // testa se tem algum lugar com esse texto
+      expect(message).toHaveLength(5); //testa se tem 5 campos
+    });
+
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+
+    await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    userEvent.type(nameInput, 'Computador'); //simula a digitação no campo do nome do produto
+    userEvent.type(priceInput, '1234.56'); //simula a digitação no campo do preço do produto
+    userEvent.type(
+      imgUrlInput,
+      'https://static-cse.canva.com/blob/183499/IMAGE-1.jpg'
+    ); //simula a digitação no campo do imagem do produto
+    userEvent.type(descriptionInput, 'Computador muito bom'); //simula a digitação no campo do descrição do produto
+
+    await waitFor(() => {
+        const message = screen.queryAllByText('Campo obrigatório'); // testa se tem algum lugar com esse texto
+        expect(message).toHaveLength(0); //testa se tem 5 campos
+      });
+
   });
 });
